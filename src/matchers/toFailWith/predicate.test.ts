226724dafc4cd38310e2c6348d65ae2645a630d8
Erik Fortune <pc@erik.org>
2020-08-02 22:11:06 -0700

    add toFailWithDetail

--- src/matchers/toFailWith/predicate.test.ts
+++ src/matchers/toFailWith/predicate.test.ts
@@ -2,17 +2,17 @@ import { fail, succeed } from '../../ts-utils';
 import { predicate } from './predicate';
 
 describe('toFailWith', () => {
-    test('fails with a failure result and matching string, RegExp or undefined', () => {
+    test('returns true for a failure result and matching string, RegExp or undefined', () => {
         expect(predicate(fail('oops'), 'oops')).toBe(true);
         expect(predicate(fail('oops'), /o.*/i)).toBe(true);
         expect(predicate(fail(undefined as unknown as string), undefined)).toBe(true);
     });
 
-    test('fails with a success result', () => {
+    test('returns false for a success result', () => {
         expect(predicate(succeed('hello'), 'hello')).toBe(false);
     });
 
-    test('fails with a failure result but non-matching string or RegExp', () => {
+    test('returns false for a failure result but non-matching string or RegExp', () => {
         expect(predicate(fail('oops'), 'error')).toBe(false);
         expect(predicate(fail('oops'), /x.*/i)).toBe(false);
     });
